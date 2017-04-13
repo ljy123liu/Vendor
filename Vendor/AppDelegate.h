45c9eda19aa67b47deaf8ccaa41ea77fa94650f2
LIUYONG <ljy123liu@sina.com>
2017-04-13 16:15:35 +0800

    Initial Commit

---
+++ Vendor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vendor
+//
+//  Created by LIUYONG on 17/4/13.
+//  Copyright © 2017年 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vendor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Vendor
+//
+//  Created by LIUYONG on 17/4/13.
+//  Copyright © 2017年 WanJianTechnology. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
